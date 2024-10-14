f4c86432b40107fdcfcc03e3bd63d194d6676458
foysal-mahmudd <156601300+foysal-mahmudd@users.noreply.github.com>
2024-10-14 22:54:19 +0600

    Create class2.cpp

---
+++ class2.cpp
@@ -0,0 +1,41 @@
+#include<iostream> 
+
+using namespace std;
+
+int main(){
+
+   // variable and data type
+
+   int a = 5;               // here int is data type and a is variable name and 5 is the 
+                            // data which we store in varaible a;
+
+   float b=5.5;             // here float is another data type which 
+                            // store floating value or decimal value
+
+   double c= 5.5343243543;  // double is like float which
+                            // store longer decimal
+
+   char f='c';              // char data is for storing letter or 
+                            // charechter value
+
+   int num1;
+   cout<<sizeof(num1);      // here we printed the size of num1
+                            // variable by using sizeof() function
+                            // you can also check for other data type
+
+   cout<<sizeof(char);      // also we can see our data type size by
+                            // direcly                      
+
+   int var1;
+   cin>>var1;               // we use cin>> fucntion to take input
+                            // from user
+   cout<<var1;              // and cout<< for printing or showing output
+                            // on screen                                  
+    
+   cout<<"hello world"<<endl;
+   cout<<"I live in bangladesh"<<endl;   // this endl keyword is for going or creat
+                                          // next line.
+   
+
+    return 0;
+}
